773996fdaf6c496d9b946a7a2057afb30fc6b58a
Ossama <ossama1998@windowslive.com>
2019-10-25 00:30:49 +0200

    creation de la classe

---
+++ src/main/java/stoneage/Zone.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stoneage;
+
+/**
+ *
+ * @author Ossama
+ */
+public class Zone {
+    private int nbOuvriersPlacés = 0;
+    private int niveauZone = 3;
+    private int constante;
+    
+    public Zone(int niveau) {
+        this.niveauZone = niveau;
+    }
+    public Zone(int niveau,int constante){
+        this.niveauZone = niveau;
+        this.constante=constante;
+    }
+    public void placerOuvrier(int nbOuvriers){
+        nbOuvriersPlacés=nbOuvriers;
+    }
+    public void resoudre(int constante){ 
+        int nb_ressources=constante/3;
+    }
+}
