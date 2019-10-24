4a91cfbc4d0c3e79118a899e0fba800a64109f0c
Balou06 <tomdonnadieu@gmail.com>
2019-10-24 17:13:08 +0200

    creation de la classe partie #8 #7

---
+++ src/main/java/stoneage/Game.java
@@ -0,0 +1,16 @@
+package stoneage;
+
+public class Game {
+	public Game() {
+		int inv=0;
+	}
+	public int getStuff() {
+		return -1;
+	}
+	
+	
+	public static final void main(String [] args) {
+		Game game=new Game();
+		System.out.println(game.getStuff());
+	}
+}
