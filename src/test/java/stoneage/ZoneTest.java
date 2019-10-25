5c4f9f32d1b9e3abb5d748a146a0704dbafcacd8
Sebastien <marrosebastien@protonmail.com>
2019-10-25 03:11:50 +0200

    Creation de la class ZoneTest #16

---
+++ src/test/java/stoneage/ZoneTest.java
@@ -0,0 +1,20 @@
+package stoneage;
+
+import stoneage.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ZoneTest {
+    private Zone zone3; // zone Foret (7 places)
+    @BeforeEach
+    void setUp(){
+        zone3 = new Zone(3);
+    }
+    @Test
+    public void placerOuvrier() {
+        zone3.placerOuvrier(1);
+        assertFalse(zone3.ouvrierDispo());
+    }
+}
