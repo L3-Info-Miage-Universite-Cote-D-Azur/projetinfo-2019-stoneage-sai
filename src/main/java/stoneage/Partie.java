2259a967f313660b0feed7e30306b30de5220b8d
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-03 18:37:16 +0100

    modification dans la class Partie #21

--- src/main/java/stoneage/Partie.java
+++ src/main/java/stoneage/Partie.java
@@ -1,48 +1,61 @@
 package stoneage;
-/**
-*
-* @author Yasmine 
-* @author Ossama
-*/
 import java.util.ArrayList;
 public class Partie {
     private final Joueur joueur;
     private final Inventaire inventaireDuJoueur;
-    private final Zone zone;
+    private final int NbZone;
+
 
 	public Partie(){
 		joueur = new Joueur();
-		zone= new Zone(3); //zone foret
-		inventaireDuJoueur=new Inventaire();
+		inventaireDuJoueur = new Inventaire();
+		NbZone=4;
+	}
+	protected void jouer(){
+        ArrayList<Zone> listeZonesDispo = new ArrayList<>();
+        ArrayList<Zone> listeZonesJouer = new ArrayList<>();
+        for (int i=3;i <= NbZone+3;i++ ){
+        	Zone zone= new Zone(i);
+        	listeZonesDispo.add(zone);
+        } //remplire la liste des zones
+        
+        inventaireDuJoueur.resetAvailableWorkers(); //remettre a jour le nombre d'ouvrier disponnible
+        System.out.println("**** phase de placement ****");
+        phasePlacement(listeZonesDispo, listeZonesJouer);
+
+        System.out.println("**** phase de résolution des ouvriers ****");
+        phaseAction(listeZonesJouer);
+
+        System.out.println("**** fin du jeu ****");
+        System.out.println("Le joueur gagne avec "+inventaireDuJoueur.getNbRessource()+ " ressource(s).");
 
 	}
-	protected void jouer(int constante){
-		Choix choix = joueur.placerOuvriers(1,1);
-		int gains = zone.resoudre(constante);
-		System.out.println("Le joueur a placé "+choix.nbOuvriersChoisie+" ouvrier(s) dans la zone "+ zone.NomZone() +".");
-		System.out.println("Le joueur gagne avec "+gains + " bois.");
-	}
-        
-        protected void phasePlacement(ArrayList<Zone> listeZonesDispo, ArrayList<Zone> listeZonesJouées){
-            while (inventaireDuJoueur.ouvrierDispo()){
-                Inventaire inventaireTemp = new Inventaire(inventaireDuJoueur);
-                Choix choix = joueur.placerOuvriers(listeZonesDispo, inventaireTemp);
-                listeZonesDispo.remove(choix.zoneChoisie);
-                listeZonesJouées.add(choix.zoneChoisie);
-                choix.zoneChoisie.placerOuvrier(inventaireDuJoueur, choix.nbOuvriersChoisie);
-                System.out.println("Le joueur a choisi la zone "+choix.zoneChoisie+" pour y placer "+choix.nbOuvriersChoisie+" ouvrier(s)");
-            }
+    protected void phaseAction(ArrayList<Zone> listeZonesJouées) {
+        while (listeZonesJouées.size() > 0 )
+        {
+            Zone choix = listeZonesJouées.get(0);
+            listeZonesJouées.remove(choix);
+            choix.resoudre(inventaireDuJoueur);
+
+            System.out.println("Le joueur reprend ses ouvriers de la zone "+choix.NomZone());
+
         }
+    }
+    protected void phasePlacement(ArrayList<Zone> listeZonesDispo, ArrayList<Zone> listeZonesJouées){
+    	while (inventaireDuJoueur.ouvrierDispo()){
+    		Inventaire inventaireTemp = new Inventaire(inventaireDuJoueur);
+    		Choix choix = joueur.placerOuvriers(listeZonesDispo, inventaireTemp);
+    		listeZonesDispo.remove(choix.zoneChoisie);
+    		listeZonesJouées.add(choix.zoneChoisie);
+    		choix.zoneChoisie.placerOuvrier(inventaireDuJoueur, choix.nbOuvriersChoisie);
+    		System.out.println("Le joueur a choisi la zone "+(choix.zoneChoisie).NomZone()+" pour y placer "+choix.nbOuvriersChoisie+" ouvrier(s)");
+    	}
+	}
         
-        
-        
-
-	
-	
 	public static final void main(String [] args) {
 		System.out.println("**** Debut de la Partie ****");
 		Partie partie = new Partie();
-		partie.jouer(6); //Choix d'une constante qui represente le dé.
+		partie.jouer(); 
 		System.out.println("**** fin de la Partie ****");		
-            }
+	}
 }
