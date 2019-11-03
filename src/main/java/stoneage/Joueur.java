bd569643a0fe8c8bdd04b6b8100804303c9ffa3a
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-03 15:27:02 +0100

    #19
    Modification de la class Joueur pour que la méthode placerOuvriers puissent faire un choix entre la liste des zones disponible

--- src/main/java/stoneage/Joueur.java
+++ src/main/java/stoneage/Joueur.java
@@ -1,10 +1,21 @@
 package stoneage;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Joueur {
-	int zone = 1;
-	int nbOuvriers = 1;
-
-	public Choix placerOuvriers(int zone, int nbOuvriers){
-		return new Choix(zone, nbOuvriers);
+	Random rand = new Random();
+	public Choix placerOuvriers(ArrayList<Zone> listeZonesDispo, Inventaire inv){
+		if ((listeZonesDispo != null) && (listeZonesDispo.size() > 0)&&inv.getNbOuvrier()>0){
+			 	int zoneChoisit = rand.nextInt(listeZonesDispo.size()); 
+				//IA simple qui choisit une zone au hazard
+				int nbOuvChoisit= rand.nextInt(inv.getNbOuvrier());
+				//IA simple qui choisit nombre d'ouvrier qu'elle va poser sur cette zone au hazard 
+				 
+	            return new Choix(zoneChoisit, nbOuvChoisit);
+		 }
+	     else 
+	    	 {
+	    	 return null;
+	    	 }
 	}
 }
