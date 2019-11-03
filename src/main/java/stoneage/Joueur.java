617a6dd24eb54c78a649fbd0e8f48cd4818eb243
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-03 15:52:20 +0100

    Correction de bug #19
    #19

--- src/main/java/stoneage/Joueur.java
+++ src/main/java/stoneage/Joueur.java
@@ -6,12 +6,12 @@ public class Joueur {
 	Random rand = new Random();
 	public Choix placerOuvriers(ArrayList<Zone> listeZonesDispo, Inventaire inv){
 		if ((listeZonesDispo != null) && (listeZonesDispo.size() > 0)&&inv.getNbOuvrier()>0){
-			 	int zoneChoisit = rand.nextInt(listeZonesDispo.size()); 
+			 	int zoneChoisie = rand.nextInt(listeZonesDispo.size()); 
 				//IA simple qui choisit une zone au hazard
-				int nbOuvChoisit= rand.nextInt(inv.getNbOuvrier());
+				int nbOuvChoisie= rand.nextInt(inv.getNbOuvrier());
 				//IA simple qui choisit nombre d'ouvrier qu'elle va poser sur cette zone au hazard 
 				 
-	            return new Choix(zoneChoisit, nbOuvChoisit);
+	            return new Choix(listeZonesDispo.get(zoneChoisie), nbOuvChoisie);
 		 }
 	     else 
 	    	 {
