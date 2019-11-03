17b947ada88bf52b553f0ed870e6e9e0fa9cf686
Ossama <ossama1998@windowslive.com>
2019-11-03 14:44:50 +0100

    changement de nom de la methode en Lancer()

--- src/main/java/stoneage/Dé.java
+++ src/main/java/stoneage/Dé.java
@@ -3,7 +3,7 @@ import java.util.Random; //importer la Class Random du package util de java
 
 public class Dé {
     private Random dé = new Random();
-    public int LancerDé() { //cette methode retourne un entier entre 1 et 6.
+    public int Lancer() { //cette methode retourne un entier entre 1 et 6.
         return dé.nextInt(6)+1; //utiliser la methode nextInt(n) qui donne un nombre au hazare ente 0 et n exclut
     }
 }
