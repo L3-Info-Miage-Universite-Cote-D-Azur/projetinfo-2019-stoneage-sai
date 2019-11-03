f86fbfdabf56a3f5cbc77c6872da54f0a2d899d8
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-03 16:49:54 +0100

    Modification de la class Zone #20
    Completer la methode resoudre

--- src/main/java/stoneage/Zone.java
+++ src/main/java/stoneage/Zone.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stoneage;
 
-/**
- *
- * @author Ossama
- * @author Yasmine 
- */
+
 public class Zone {
     private int nbOuvriersPlacés = 0;
     private int niveauZone = 3;
