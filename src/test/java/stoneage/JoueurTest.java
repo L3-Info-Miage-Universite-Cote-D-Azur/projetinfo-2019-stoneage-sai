2e5ba66c226742aaa27eeb1080d007993e4d00a7
Ossama <ossama1998@windowslive.com>
2019-11-07 22:55:08 +0100

    Creation du test de la classe joueur #37

--- src/test/java/stoneage/JoueurTest.java
+++ src/test/java/stoneage/JoueurTest.java
@@ -5,17 +5,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 public class JoueurTest {
-    private Choix choix;
-    private Joueur j;
+    ArrayList<Zone> listeZone ;
+    Zone zone;
+    Choix choix ,choix2;
+    Joueur j;
+    Inventaire inventaire;
+    
     @BeforeEach
     void setUp(){
-        choix = new Choix(3,1);
+        listeZone = new ArrayList<>();
+        for (int i = 3; i <=6; i++) {
+            zone = new Zone(i);
+            listeZone.add(zone);
+        }
         j = new Joueur();
+	inventaire = new Inventaire();      
+        choix2=j.placerOuvriers(listeZone, inventaire);
+        choix = new Choix(choix2.zoneChoisie,choix2.nbOuvriersChoisie);//je crée un objet avec les valeurs 
+        //zoneChoisie et nbOuvriersChoisie renvoyé par j.placerOuvriers(listeZone, inventaire)par ce que 
+        //leur création se fait avec un random et c'est le seul moyen de les recupérées .
     }
-    @Test
-    public void placerOuvrier() {
-        assertEquals(choix, j.placerOuvriers(3, 1));
+	@Test
+    void placerOuvrier() {
+            assertEquals(choix, choix2);   
     }
 }
