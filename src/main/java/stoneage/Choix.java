fff7438005538d4dc961f28dcf721dcd33d53ae6
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-11 22:10:48 +0100

    Completer la class Choix #61

--- src/main/java/stoneage/Choix.java
+++ src/main/java/stoneage/Choix.java
@@ -3,11 +3,17 @@ package stoneage;
 public class Choix {
 	public Zone zoneChoisie;
 	public int nbOuvriersChoisie;
+	public int nbOutils;
 	
 	public Choix(Zone zoneJouer, int nbOuvriersJouer) { //constructeur qui fait :
 		this.zoneChoisie = zoneJouer;               //l'affectation de la zoneChoisie 
 		this.nbOuvriersChoisie = nbOuvriersJouer;   //l'affectation des nbOuvriersChoisie
+		
 	}
+	public Choix(int nbOutils){
+		this.nbOutils=nbOutils;		
+	}
+	 
 	 public boolean equals(Object o) {
 	     if (o instanceof Choix) {
 	    	 
