ccde2f214f0e584590a5d85da7af5cd71834a164
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-11 23:11:07 +0100

    Modification de la class Joueur #57

--- src/main/java/stoneage/Joueur.java
+++ src/main/java/stoneage/Joueur.java
@@ -4,6 +4,46 @@ import java.util.Random;
 
 public class Joueur {
 	Random rand = new Random();
+    public void recupeRes(Inventaire inventaireJoueur, Zone zoneChoisi) { 
+    	int nbRessources= zoneChoisi.lancéDeDés(zoneChoisi.getNbOuvriersPlaces());
+
+    	if ((nbRessources % zoneChoisi.niveauZone)<inventaireJoueur.getNbOutils())
+    	{
+    		nbRessources+=nbRessources % zoneChoisi.niveauZone;
+    		nbRessources=nbRessources / zoneChoisi.niveauZone;
+    		inventaireJoueur.setNbOutils(inventaireJoueur.getNbOutils()-(nbRessources % zoneChoisi.niveauZone));	   	
+    	}    		
+    	//recuperer les ressources gagner
+    	inventaireJoueur.setNbRessource(inventaireJoueur.getNbRessource()+nbRessources);
+    	//ajouter les nouveau ressources a l'inventaire du joueur 
+    	switch(zoneChoisi.niveauZone)
+    	{
+    		case 1:
+    			inventaireJoueur.setNbOutils(inventaireJoueur.getNbOutils()+1);
+    		case 2:
+    			inventaireJoueur.setNourriture(inventaireJoueur.getNourriture()+nbRessources);
+    		case 3:
+    			inventaireJoueur.setNbBois(inventaireJoueur.getNbBois()+nbRessources);
+    			break;
+    		case 4: 
+    			inventaireJoueur.setNbArgile(inventaireJoueur.getNbArgile()+nbRessources);
+    			break;
+    		case 5:
+    			inventaireJoueur.setNbPierre(inventaireJoueur.getNbPierre()+nbRessources);
+    			break;
+
+    		case 6:
+    			inventaireJoueur.setNbOr(inventaireJoueur.getNbOr()+nbRessources);
+    			break;
+    		default:
+    			break;			
+    	}
+        inventaireJoueur.addAvailableWorkers(zoneChoisi.getNbOuvriersPlaces());
+        //recuperer les ouvriers 
+        zoneChoisi.resetNbOuvriersPlaces();
+        zoneChoisi.setNbPlaceDispo(zoneChoisi.getNbPlaceZone());//quand on recupere les ouvriers,toutes les places deviennent disponibles.    
+    }
+    
 	public Choix placerOuvriers(ArrayList<Zone> listeZonesDispo, Inventaire inv){
 		if ((listeZonesDispo != null) && (listeZonesDispo.size() > 0))
 			{
