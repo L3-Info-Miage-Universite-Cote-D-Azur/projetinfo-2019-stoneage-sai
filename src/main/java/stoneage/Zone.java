a9774bd402b8d6e4cec1100d319057d1d4712aa2
Ossama <ossama1998@windowslive.com>
2019-11-11 21:30:36 +0100

    Modification de la classe Zone #54

--- src/main/java/stoneage/Zone.java
+++ src/main/java/stoneage/Zone.java
@@ -3,30 +3,51 @@ package stoneage;
 
 public class Zone {
     private int nbOuvriersPlacés = 0;
-    public int niveauZone = 3;
+    public int niveauZone ;
     private Dé dé;
+    private int nbPlaceZone ;
+    private int nbPlaceDispo;
     public Zone(int niveau) {
         this.niveauZone = niveau;
         dé=new Dé();
+        switch(niveau){
+            case 1:
+                nbPlaceZone=1;
+            case 2:
+                nbPlaceZone=100;//le nombre de place dans la zone chasse est illimité
+            case 3:case 4:case 5:case 6:
+                nbPlaceZone=7;  
+        }
+        nbPlaceDispo=nbPlaceZone;//au début le nombre de place disponible = au nombre place max de la zone
     }
     public Zone(int niveau,Dé dé){
         this.niveauZone = niveau;
         this.dé=dé;
     }
+    public int getNbPlaceZone(){
+        return nbPlaceZone;
+    }
+    public int getNbPlaceDispo(){
+        return nbPlaceDispo;
+    }
     public void placerOuvrier(Inventaire inventaireJoueur,int nbOuvriers){
-    	if (nbOuvriers>=1 && nbOuvriers <=5){
+    	if (nbOuvriers>=1 && nbOuvriers <=nbPlaceDispo){
     		inventaireJoueur.removeAvailableWorkers(nbOuvriers);//pour placer un nbOuvrier il faut les retirer d'abord de l'inventaire du joueur  
     		nbOuvriersPlacés=nbOuvriers;   
+                nbPlaceDispo=nbPlaceZone-nbOuvriersPlacés;
     	}
     }
 
-    public void resoudre(Inventaire inventaireJoueur) { 
+    public void resoudre(Inventaire inventaireJoueur){ 
     	int nbRessources= lancéDeDés(nbOuvriersPlacés);
     	//recuperer les ressources gagner
     	inventaireJoueur.setNbRessource(inventaireJoueur.getNbRessource()+nbRessources);
     	//ajouter les nouveau ressources a l'inventaire du joueur 
-    	switch(niveauZone)
-    	{
+    	switch(niveauZone){
+                case 1:
+                        inventaireJoueur.setNbOutils(inventaireJoueur.getNbOutils()+1);
+                case 2:
+                        inventaireJoueur.setNourriture(inventaireJoueur.getNourriture()+nbRessources);
     		case 3:
     			inventaireJoueur.setNbBois(inventaireJoueur.getNbBois()+nbRessources);
     			break;
@@ -46,11 +67,11 @@ public class Zone {
         inventaireJoueur.addAvailableWorkers(nbOuvriersPlacés);
         //recuperer les ouvriers 
         nbOuvriersPlacés=0;
-        
+        nbPlaceDispo=nbPlaceZone;//quand on recupere les ouvriers,toutes les places deviennent disponibles.
     }
 
     public String NomZone(){
-    	String[] nomZone={"Agriculture","Chasse","foret","glaisière","carrière","rivière"};//ajout des zones glaisière,carrière,rivière
+    	String[] nomZone={"Fabrication d'Outils","Chasse","foret","glaisière","carrière","rivière"};//ajout des zones glaisière,carrière,rivière
     	String nom=nomZone[niveauZone - 1];
     	return nom;
     }
