2e1d972ab0bd09455140d0a2e053df9e6fd9e6ba
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-15 01:41:18 +0100

    Modification de la class #54

--- src/main/java/stoneage/Zone.java
+++ src/main/java/stoneage/Zone.java
@@ -1,113 +1,76 @@
 package stoneage;
-import java.util.ArrayList;
+
 
 public class Zone {
     private int nbOuvriersPlacés = 0;
-    public int niveauZone;
-    private Dé dice;
-
-    private static ArrayList<Zone> listeZone = new ArrayList<Zone>();
-    private static ArrayList<Boolean> listeZoneDispo = new ArrayList<Boolean>();
-    private static ArrayList<Boolean> listeZoneJouee = new ArrayList<Boolean>();
-    private static ArrayList<Integer> placeDispoParZone = new ArrayList<Integer>();
-
-    private Zone zone1;
-    private Zone zone2;
-    private Zone zone3;
-    private Zone zone4;
-    private Zone zone5;
-    private Zone zone6;
+    public int niveauZone ;
+    private Dé dé;
+    private int nbPlaceZone ;
+    private int nbPlaceDispo;
     public Zone(int niveau) {
-        initializeZone();
         this.niveauZone = niveau;
-        dice = new Dé();
+        dé=new Dé();
+        switch(niveau){
+            case 1:
+                nbPlaceZone=1;
+            case 2:
+                nbPlaceZone=100;//le nombre de place dans la zone chasse est illimité
+            case 3:case 4:case 5:case 6:
+                nbPlaceZone=7;  
+        }
+        nbPlaceDispo=nbPlaceZone;//au début le nombre de place disponible = au nombre place max de la zone
     }
-
-    public Zone(int niveau, Dé dice){
-        initializeZone();
+    public Zone(int niveau,Dé dé){
         this.niveauZone = niveau;
-        this.dice=dice;
+        this.dé=dé;
     }
-    public void initializeZone(){
-        listeZone.add(zone1);
-        listeZone.add(zone2);
-        listeZone.add(zone3);
-        listeZone.add(zone4);
-        listeZone.add(zone5);
-        listeZone.add(zone6);
-
-        for(int i=0; i< 6 ;i++) {
-            listeZoneDispo.add(true); //true si disponible, false sinon
-            listeZoneJouee.add(false);
-        }
-
-        placeDispoParZone.add(1);
-        placeDispoParZone.add(100);
-        placeDispoParZone.add(7);
-        placeDispoParZone.add(7);
-        placeDispoParZone.add(7);
-        placeDispoParZone.add(7);
+    
+    public int getNbPlaceZone(){
+        return nbPlaceZone;
     }
-    public void placerOuvrier(Inventaire inventaireJoueur, int nbOuvriers){
-    	if (nbOuvriers>=1 && nbOuvriers <= placeDispoParZone.get(niveauZone - 1) && nbOuvriers <=5 ){
-    		inventaireJoueur.removeAvailableWorkers(nbOuvriers);//pour placer un nbOuvrier il faut les retirer d'abord de l'inventaire du joueur
-            placeDispoParZone.set(niveauZone - 1, placeDispoParZone.get(niveauZone - 1) - nbOuvriers);
-            nbOuvriersPlacés = nbOuvriers;
-            removeZoneDispo();
-        }
+    public int getNbPlaceDispo(){
+        return nbPlaceDispo;
     }
-    public void removeZoneDispo(){
-        // On mets false quand une zone est complete
-        // pour ne pas supprimer et garder les indices dans le bon ordre
-        if (placeDispoParZone.get(niveauZone - 1) <= 0){
-            listeZoneDispo.set(niveauZone - 1,false);
-        }
+    public void setNbPlaceDispo(int nbPlaceDispo)
+    {
+    	this.nbPlaceDispo=nbPlaceDispo;
+    }
+    public void placerOuvrier(Inventaire inventaireJoueur,int nbOuvriers){
+    	if (nbOuvriers>=1 && nbOuvriers <=nbPlaceDispo && nbOuvriers<=5 ){
+    		inventaireJoueur.removeAvailableWorkers(nbOuvriers);//pour placer un nbOuvrier il faut les retirer d'abord de l'inventaire du joueur  
+    		nbOuvriersPlacés=nbOuvriers;   
+            nbPlaceDispo=nbPlaceDispo-nbOuvriersPlacés;
+    	}
     }
 
     public int getNbOuvriersPlaces(){
     	return nbOuvriersPlacés;
     }
-
     public void resetNbOuvriersPlaces(){
     	nbOuvriersPlacés=0;
     }
+
     public String NomZone(){
     	String[] nomZone={"Fabrication d'Outils","Chasse","foret","glaisière","carrière","rivière"};//ajout des zones glaisière,carrière,rivière
     	String nom=nomZone[niveauZone - 1];
     	return nom;
     }
-    //retourne une valeur booléenne pour vérifier si tous les ouvriers ont été placés ou non.
-
-    public boolean ouvrierPlace(int nbOuvriers){
+    
+    //retourne une valeur booléenne pour vérifier si tous les ouvrier ont était placer ou non.
+    public boolean ouvrierPlace(int nbOuvriers){ 
         return (nbOuvriersPlacés == nbOuvriers);
     }
+    
     /*on lance autant de Dés que des nbOuvriersPlacés
     et on retourne la somme des Dés jetées divisé par niveauZone
     */
-
-    public int lancéDeDés(int nbOuvriersPlacés){
-        int sommeDés=0;
+    public int lancéDeDés(int nbOuvriersPlacés){  
+        int sommeDés=0;                            
         for (int i = 0; i < nbOuvriersPlacés; i++) {
-            sommeDés+= dice.Lancer();
+            sommeDés+=dé.Lancer();
         }
         return sommeDés ;
     }
-    public static ArrayList<Zone> getListeZone() {
-        return listeZone;
-    }
-
-    public static ArrayList<Integer> getPlaceDispoParZone() {
-        return placeDispoParZone;
-    }
-
-    public static ArrayList<Boolean> getListeZoneDispo() {
-        return listeZoneDispo;
-    }
-
-    public static ArrayList<Boolean> getListeZoneJouee() {
-        return listeZoneJouee;
-    }
-
     @Override
     public String toString(){
         return NomZone();
