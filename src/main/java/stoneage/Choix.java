0b8e4f84e37b8e38f96a03ae0e6c2336015a6a96
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-15 01:42:47 +0100

    Modification de la class Choix #61

--- src/main/java/stoneage/Choix.java
+++ src/main/java/stoneage/Choix.java
@@ -1,39 +1,26 @@
 package stoneage;
 
 public class Choix {
-	private Zone getZoneChoisie;
-	private int nbOuvriersChoisis;
-	private int nbOutils;
-
+	public Zone zoneChoisie;
+	public int nbOuvriersChoisie;
+	public int nbOutils;
+	
 	public Choix(Zone zoneJouer, int nbOuvriersJouer) { //constructeur qui fait :
-		this.getZoneChoisie = zoneJouer;               //l'affectation de la zoneChoisie
-		this.nbOuvriersChoisis = nbOuvriersJouer;   //l'affectation des nbOuvriersChoisie
-
+		this.zoneChoisie = zoneJouer;               //l'affectation de la zoneChoisie 
+		this.nbOuvriersChoisie = nbOuvriersJouer;   //l'affectation des nbOuvriersChoisie
+		
 	}
+	 
 	 public boolean equals(Object o) {
 	     if (o instanceof Choix) {
-
+	    	 
 	         Choix c = (Choix) o;
-	            return (c.nbOuvriersChoisis == nbOuvriersChoisis) && (c.getZoneChoisie.niveauZone == getZoneChoisie.niveauZone);
+	            return (c.nbOuvriersChoisie == nbOuvriersChoisie) && (c.zoneChoisie.niveauZone == zoneChoisie.niveauZone);
 	        }
 	        else return false;
 	    }
-
-	@Override
-	public String toString(){
-             return "nbOuvriersChoisie="+nbOuvriersChoisis+"  zoneChoisie="+ getZoneChoisie;
-	}
-
-
-	public Zone getZoneChoisie() {
-		return getZoneChoisie;
-	}
-
-	public int getNbOuvriersChoisis() {
-		return nbOuvriersChoisis;
-	}
-
-	public int getNbOutils() {
-		return nbOutils;
-	}
+        @Override
+         public String toString(){
+             return "nbOuvriersChoisie="+nbOuvriersChoisie+"  zoneChoisie="+zoneChoisie;
+         }
 }
