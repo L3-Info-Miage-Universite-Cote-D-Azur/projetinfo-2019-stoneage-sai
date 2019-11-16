330b9856ec9de7c3688d2f9bb829ab07838f676f
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2019-11-17 00:18:25 +0100

    Modification de la classe PartieTest #56

--- src/test/java/stoneage/PartieTest.java
+++ src/test/java/stoneage/PartieTest.java
@@ -14,32 +14,85 @@ public class PartieTest {
 	private int nbZone;
 
     private Inventaire  inv;
-    private Joueurs joueur;
+    private Joueurs joueur, joueurIA;
    
     @BeforeEach
     void setUp(){
-    	inv=new Inventaire();
-    	joueur=new Joueur();
-    	nbZone=4;
+    	inv = new Inventaire();
+    	joueur = new Joueur();
+    	joueurIA = new JoueurIA();
+    	nbZone = 6;
     	//retirer 2 ouvrier de ouvrier dispo pour le teste de PhaseAction
-    	partie =new Partie(nbZone);
+    	partie = new Partie(nbZone);
     }
 
 	@Test	
-	public void testPhaseAction() {	
-		assertEquals(inv.listeZonesDispo.size(),4); 
-		partie.phaseAction(inv,joueur,1);
-		// cette phase prend chaque zone dans la liste et l'efface apres avoir fini avec 
-		assertEquals(inv.listeZonesDispo.size(),0); //verifier si cette methode a bien vider la liste 		
+	public void testPhaseAction() {
+		assertEquals(inv.listeZonesDispo.size(), 6); //il y a 6 zones
+		
+		for(int i = 0; i < 6 ;i++){
+			inv.listeZonesJouer.add(true); //On initialise toute les zones a True
+		}
+		partie.phaseAction(inv, joueur, 1); //La fonction doit remettre False a toutes les zones
+		for(int i = 0; i < 6; i++){
+			assertFalse(inv.listeZonesJouer.get(i));  //On vérifie si toutes les zones sont a False
+		}	
 	}
 
 	@Test
 	public void testPhasePlacement() {
-		assertEquals(inv.listeZonesDispo.size(),4); 
-		assertEquals(inv.listeZonesJouer.size(),0); 
+		inv.setNourriture(0);
+		assertEquals(inv.listeZonesDispo.size(),6); 
 
-		partie.phasePlacement(inv,joueur,1);
-		assertTrue(4-inv.listeZonesDispo.size()==inv.listeZonesJouer.size()); 
+		partie.phasePlacement(inv, joueurIA, 1); //Comme le JoueurIA n'a pas de nourriture il doit jouer la zone chasse
+		assertFalse(inv.listeZonesJouer.get(2)); // "listeZonesJouer.get(2)" est la zone chasse
 	}
-
+	
+	
+	@Test
+	public void testPhaseNourrir() {
+		joueur = new Joueur();
+		
+		inv.resetInventory();
+		partie.phaseNourrir(inv, joueur, 1);
+		assertEquals(inv.getNourriture(), 10);
+		
+		
+		inv.resetInventory();
+		inv.setNourriture(0);
+		inv.setScore(30);     //On initialise le score du joueur a 30
+		partie.phaseNourrir(inv, joueur, 1);  //Comme le joueur n'a pas assez de nourriture et de ressource pour nourrire ses ouvriers, il a une pénalité de 10 points
+		assertEquals(inv.getScore(), 20);     //Le score du joueur doit donc etre de 20
+		
+		
+		inv.resetInventory();
+		inv.setNourriture(0);
+		inv.setNbBois(10);
+		partie.phaseNourrir(inv, joueur, 1);  //Le joueur utilise sont bois pour nourrire ses ouvriers
+		assertEquals(inv.getNbBois(), 5);
+		
+		
+		inv.resetInventory();
+		inv.setNourriture(0);
+		inv.setNbBois(3);
+		inv.setNbArgile(10);
+		partie.phaseNourrir(inv, joueur, 1);  //Le joueur utilise 3 bois et 2 argiles pour nourrire ses ouvriers
+		assertEquals(inv.getNbBois(), 0);
+		assertEquals(inv.getNbArgile(), 8);
+		
+		
+		inv.resetInventory();
+		inv.setNourriture(0);
+		inv.setNbBois(1);
+		inv.setNbArgile(1);
+		inv.setNbPierre(1);
+		inv.setNbOr(10);
+		partie.phaseNourrir(inv, joueur, 1);  //Le joueur utilise le bois, l'argile, la pierre et 2 or pour nourrire ses ouvriers
+		assertEquals(inv.getNbBois(), 0);
+		assertEquals(inv.getNbArgile(), 0);
+		assertEquals(inv.getNbPierre(), 0);
+		assertEquals(inv.getNbOr(), 8);
+		
+	}
+	
 }
