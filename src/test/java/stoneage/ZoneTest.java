2cc94ab01f55910796700691c06c306ec70a732d
Sebastien <marrosebastien@protonmail.com>
2019-11-16 21:47:23 +0100

    Test de la methode placerOuvrier() #55

--- src/test/java/stoneage/ZoneTest.java
+++ src/test/java/stoneage/ZoneTest.java
@@ -1,38 +1,48 @@
 package stoneage;
 
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.* ;
 
 @ExtendWith(MockitoExtension.class)
 public class ZoneTest {
-    private Zone zone1; // zone Fabrivation outil (1 places)
-    private Zone zone2; // zone Chasse (100 places)
-    private Zone zone3; // zone Foret (7 places)
-    private Zone zone4; // zone Glaisière (7 places)
-    private Zone zone5; // zone Carrière (7 places)
-    private Zone zone6; // zone Rivière (7 places)
-    private Zone[] listZone;
-    Inventaire inventaire = new Inventaire();
-    @Mock
-    Dé unDéTruqué;
+    Zone zone1;     // Fabrication d'outils
+    Zone zone2;     // Chasse
+    Zone zone3;     // Foret
+    Zone zone4;     // Glaisiere
+    Zone zone5;     // Carriere
+    Zone zone6;     // Riviere
+    Zone[] listeZone = new Zone[6];
+    Inventaire inventaire;
 
+    @Mock
+    Dé dice;
 
     @BeforeEach
     void setUp(){
-
         zone1 = new Zone(1);
         zone2 = new Zone(2);
         zone3 = new Zone(3);
         zone4 = new Zone(4);
         zone5 = new Zone(5);
         zone6 = new Zone(6);
-        listZone = new Zone[]{ zone1 , zone2, zone3, zone4, zone5, zone6};
+
+        listeZone[0] = zone1;
+        listeZone[1] = zone2;
+        listeZone[2] = zone3;
+        listeZone[3] = zone4;
+        listeZone[4] = zone5;
+        listeZone[5] = zone6;
+
+        inventaire = new Inventaire();
     }
 
     /* TEST 1: placement des ouvriers
@@ -40,23 +50,27 @@ public class ZoneTest {
      * car on a maximum 5ouvrier .
      * Et on ne doit pas pouvoir placer 0 ouvrier sur une zone.
      *
-     *
-     * TEST 2: resoudre:
-     * Avoir un nombre de ressources correspondant à la zone et au nombre d'ouvriers
-     * */
+     */
 
     @Test
-    public void placerOuvrier() {
-        for(Zone zone : listZone){
-            for(int j=0; j<7 ;j++){
-                zone.placerOuvrier(inventaire, j);
-                if((j <= 5) && ( j> 0)){
-                    assertTrue(zone.ouvrierPlace(j));
+    public void placerOuvrier(){
+        for(Zone zones : listeZone){
+            for (int i = 1; i < 6; i++){
+                zones.placerOuvrier(inventaire, i);
+                if (zones == zone1 && i>1) { // zone1, une seule place donc on sépare ce case des autres
+                    assertNotEquals(i, zones.getNbOuvriersPlaces());
+                }else{
+                    assertEquals(i, zones.getNbOuvriersPlaces());
                 }
-                else{
-                    assertFalse(zone.ouvrierPlace(j));
-                }
+                //reset des zones et de l'inventaire
+                zones.resetNbOuvriersPlaces();
+                zones.setNbPlaceDispo(zones.getNbPlaceZone());
+                inventaire.resetAvailableWorkers();
             }
         }
+    }
+
+    @Test
+    public void throwingDice() {
     }
 }
