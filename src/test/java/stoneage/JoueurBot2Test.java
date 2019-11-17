dc275d78f8c33d7179c8815ba7f2ce8639bbe1e0
Sebastien <marrosebastien@protonmail.com>
2019-11-17 01:19:14 +0100

    Premier test de placer Ouvrier, nourriture insuffisante. #67 2but deuxieme test, nourriture suffisante et zone outils dispo. #67

--- src/test/java/stoneage/JoueurBot2Test.java
+++ src/test/java/stoneage/JoueurBot2Test.java
@@ -1,46 +1,70 @@
 package stoneage;
 
-import stoneage.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
-import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
 
 
 public class JoueurBot2Test {
-	    Zone zone;
-	    Choix choix ,choix2;
-	    JoueurBot2 j;
-	    Inventaire inventaire , inventaire2;
-	     
+    ArrayList<Zone> listeZones = new ArrayList<Zone>();
+    Zone zone1, zone2, zone3, zone4, zone5, zone6;
+    Choix actualChoice ,expectedChoice;
+    JoueurBot2 joueurBot;
+    Inventaire inventaire;
 
 
-	    @BeforeEach
-	    	void setUp(){
-	        j = new JoueurBot2();
-	        inventaire = new Inventaire();
-	        inventaire2 = new Inventaire();
-	    }
+    @BeforeEach
+    void setUp(){
+        zone1 = new Zone(1);
+        zone2 = new Zone(2);
+        zone3 = new Zone(3);
+        zone4 = new Zone(4);
+        zone5 = new Zone(5);
+        zone6 = new Zone(6);
+
+        listeZones.add(zone1);
+        listeZones.add(zone2);
+        listeZones.add(zone3);
+        listeZones.add(zone4);
+        listeZones.add(zone5);
+        listeZones.add(zone6);
+
+        joueurBot = new JoueurBot2();
+        inventaire = new Inventaire();
+    }
 
 
-	    @Test
-	    void placerOuvrier() {
-	        //Si 3 < nbNourriture < 5, alors on choisi la zone 2 en placant le nombre d'ouvriers dispo.
-	        inventaire.setNourriture(4);
-	        choix2 = j.placerOuvriers(inventaire);
-	        zone = new Zone(2); 
-	        choix = new Choix(zone, 1);  //1 = NbouvriersDispo
-	        assertEquals(choix, choix2);
-	        
-	      //Si  nbNourriture < 3, alors on choisi la zone 2 en placant le nombre d'ouvriers dispo.
-	        inventaire.setNourriture(2);
-	        choix2 = j.placerOuvriers(inventaire);
-	        zone = new Zone(2); 
-	        choix = new Choix(zone, 3);  //3 = NbouvriersDispo
-	        assertEquals(choix, choix2);
+    @Test
+    void placerOuvrierNourritureInsuffisante() {
+        for(int i=0; i< 5 ; i++){
+            inventaire.setNourriture(i);
+            if(i<3){
+                expectedChoice = new Choix(1, 3);
+                actualChoice = joueurBot.placerOuvriers(listeZones, inventaire);
+                assertEquals(expectedChoice, actualChoice);
+            }
+            else{
+                expectedChoice = new Choix(1, 1);
+                actualChoice = joueurBot.placerOuvriers(listeZones, inventaire);
+                assertEquals(expectedChoice, actualChoice);
+            }
+        }
+    }
 
-	    }
+    @Test
+    void placerOuvrierNourritureSuffisante() {
+        inventaire.setNourriture(6);
+        if(zone1.getNbPlaceZone() == 1){
+            expectedChoice = new Choix(0, 1);
+            actualChoice = joueurBot.placerOuvriers(listeZones, inventaire);
+            assertEquals(expectedChoice, actualChoice);
+        }
+    }
+    /*
 	    @Test
 	    void recupRes() {
 	    	// si zone = 1 recupere 1 outil
@@ -48,7 +72,7 @@ public class JoueurBot2Test {
 	    	zone = new Zone(1);
 	    	j.recupeRes(inventaire2, zone);
 	    	assertEquals(inventaire.getNbOutils(),inventaire2.getNbOutils());
-	    	
-	    }
+	    }*/
 
 }
+
