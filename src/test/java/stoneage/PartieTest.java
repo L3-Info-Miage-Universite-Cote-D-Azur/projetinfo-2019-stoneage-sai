7c1e0fa3d083b309d89381063cce223291dbda25
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2019-11-20 22:19:42 +0100

    Modification PartieTest #76

--- src/test/java/stoneage/PartieTest.java
+++ src/test/java/stoneage/PartieTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PartieTest {
 	private  Partie partie;
-	private int nbZone;
 
     private Inventaire  inv;
     private Joueurs joueur, joueurIA;
@@ -21,9 +20,8 @@ public class PartieTest {
     	inv = new Inventaire();
     	joueur = new Joueur();
     	joueurIA = new JoueurIA();
-    	nbZone = 6;
     	//retirer 2 ouvrier de ouvrier dispo pour le teste de PhaseAction
-    	partie = new Partie(nbZone);
+    	partie = new Partie();
     }
 
 	@Test	
@@ -66,19 +64,23 @@ public class PartieTest {
 		
 		
 		inv.resetInventory();
-		inv.setNourriture(0);
+		inv.setNourriture(1);
 		inv.setNbBois(10);
+		inv.setNbRessource(10);
 		partie.phaseNourrir(inv, joueur, 1);  //Le joueur utilise sont bois pour nourrire ses ouvriers
-		assertEquals(inv.getNbBois(), 5);
+		assertEquals(inv.getNbBois(), 6);
+		assertEquals(inv.getNbRessource(), 6);
 		
 		
 		inv.resetInventory();
 		inv.setNourriture(0);
 		inv.setNbBois(3);
 		inv.setNbArgile(10);
+		inv.setNbRessource(13);
 		partie.phaseNourrir(inv, joueur, 1);  //Le joueur utilise 3 bois et 2 argiles pour nourrire ses ouvriers
 		assertEquals(inv.getNbBois(), 0);
 		assertEquals(inv.getNbArgile(), 8);
+		assertEquals(inv.getNbRessource(), 8);
 		
 		
 		inv.resetInventory();
@@ -87,11 +89,28 @@ public class PartieTest {
 		inv.setNbArgile(1);
 		inv.setNbPierre(1);
 		inv.setNbOr(10);
+		inv.setNbRessource(13);
 		partie.phaseNourrir(inv, joueur, 1);  //Le joueur utilise le bois, l'argile, la pierre et 2 or pour nourrire ses ouvriers
 		assertEquals(inv.getNbBois(), 0);
 		assertEquals(inv.getNbArgile(), 0);
 		assertEquals(inv.getNbPierre(), 0);
 		assertEquals(inv.getNbOr(), 8);
+		
+		
+		
+		inv.resetInventory();
+		inv.setNourriture(1);
+		inv.setNbBois(0);
+		inv.setNbArgile(1);
+		inv.setNbPierre(4);
+		inv.setNbOr(10);
+		inv.setNbRessource(15);
+		partie.phaseNourrir(inv, joueur, 1);
+		assertEquals(inv.getNbBois(), 0);
+		assertEquals(inv.getNbArgile(), 0);
+		assertEquals(inv.getNbPierre(), 1);
+		assertEquals(inv.getNbOr(), 10);
+		assertEquals(inv.getNbRessource(), 11);
 		
 	}
 	
