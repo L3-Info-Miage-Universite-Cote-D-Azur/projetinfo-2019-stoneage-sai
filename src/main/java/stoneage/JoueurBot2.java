9c4666c7deea493090d28743d80e70c9ed3ae3d2
Balou06 <tomdonnadieu@gmail.com>
2019-11-21 17:23:19 +0100

    ameliration de JoueurBot2 #79

--- src/main/java/stoneage/JoueurBot2.java
+++ src/main/java/stoneage/JoueurBot2.java
@@ -71,48 +71,44 @@ public class JoueurBot2 implements Joueurs {
  	* Place un ouvrier en fonction de ses ressources et de son nombre d'ouvrier dispo      *
  	* Si les conditions ne marchent pas pour placer ses ouvrier il les place aléatoirement *
     ****************************************************************************************/
-	public Choix placerOuvriers(ArrayList<Zone> LesZones ,Inventaire inv){
-		
-		if ( (inv.getNbZoneJouer() < 6&&inv.ouvrierDispo())){
+		public Choix placerOuvriers(ArrayList<Zone> LesZones ,Inventaire inv){
+			
+			if ( (inv.getNbZoneJouer() < 6 && inv.ouvrierDispo())){
 
-			if (inv.listeZonesJouer.get(1)==false&&inv.getNourriture() >= 3  && LesZones.get(1).getNbPlaceDispo()>1&& inv.getNourriture() < 5)//Chasse
-			{	//SI la zone fabrication d'outil n'est pas etuliser 
-				//si elle a plus que 1 place 
-				//si le joueur a moins que 5 nourriture 
-				return new Choix(1, 1);
-				//on choisi cette zone et on pose 1 ouvrier la dedans 
-			}
-			else if (inv.listeZonesJouer.get(1)==false&&inv.getNourriture() < 3&& LesZones.get(1).getNbPlaceDispo()>3) {
-				return new Choix(1, 3);
-			}
-			else {
-				if (inv.listeZonesJouer.get(0)==false&& LesZones.get(0).getNbPlaceDispo()>=1) { //Fabrication d'outils
-				
-					return new Choix(0, 1);
+				if (inv.listeZonesJouer.get(1)==false && inv.getNourriture() >= 5 && inv.getNourriture() <10   && LesZones.get(1).getNbPlaceDispo()>2 )//Chasse
+				{	//SI la zone chasse n'est pas etuliser 
+					//si elle a plus que 2 place 
+					//si le joueur 5 > nourriture > 3 
+					return new Choix(1, 1);
+					//on choisi cette zone et on pose 2 ouvrier la dedans 
 				}
-				else if(inv.listeZonesJouer.get(5)==false&& LesZones.get(5).getNbPlaceDispo()>=5 && inv.getNbOr() < 4 &&  inv.getNbOuvrierDispo() == 5) {//Riviere
-					return new Choix(5, 5);
+				else if (inv.listeZonesJouer.get(1)==false && inv.getNourriture() < 3 && LesZones.get(1).getNbPlaceDispo()>3) {
+					// SI nourriture < 3
+					return new Choix(1, 3);
 				}
-				else if(inv.listeZonesJouer.get(4)==false && inv.getNbPierre() < 4 && LesZones.get(4).getNbPlaceDispo()>= 4&&  inv.getNbOuvrierDispo() >= 4) { //Carriere
-					return new Choix(4, 4);
-				}
-				else if(inv.listeZonesJouer.get(3)==false && inv.getNbArgile() < 6 && LesZones.get(3).getNbPlaceDispo()>= 3&&  inv.getNbOuvrierDispo() >= 3) { //Glaisiere
-					return new Choix(3, 3);
-				}
-				else if(inv.listeZonesJouer.get(2)==false && LesZones.get(2).getNbPlaceDispo()>= 2 && inv.getNbBois() < 10 && inv.getNbOuvrierDispo() >= 2) {//Foret
-					return new Choix(2, 2);
-				}
-				else {
-					int zoneChoisie = (rand.nextInt(4) + 2);
-					int nbOuvChoisie= (Math.min(inv.getNbOuvrierDispo(),LesZones.get(zoneChoisie).getNbPlaceDispo()));
-					return new Choix (zoneChoisie, nbOuvChoisie);
+				else if (inv.listeZonesJouer.get(2)==false&& LesZones.get(2).getNbPlaceDispo()>=2 && inv.getNbBois()<10 && inv.getNbOuvrierDispo() >= 2) { 
+						return new Choix(2, 2);
+					}
+				else if(inv.listeZonesJouer.get(0)==false && LesZones.get(0).getNbPlaceDispo()>= 1 && inv.getNbOuvrierDispo() >= 1) {//Fabrication outils
+						return new Choix(0, 1);
+					}
+				else 
+				{
+					int zoneChoisie = rand.nextInt(6);
+				 	while ( inv.listeZonesJouer.get(zoneChoisie)==true || LesZones.get(zoneChoisie).getNbPlaceDispo()==0){
+				 		zoneChoisie = rand.nextInt(6);
+				 	}
 					
+					int nbOuvChoisie= rand.nextInt(Math.min(inv.getNbOuvrierDispo(),LesZones.get(zoneChoisie).getNbPlaceDispo()))+1;
+					
+		            return new Choix(zoneChoisie, nbOuvChoisie);
 				}
-			}
+				}
+			
+			
+			return null;
+			
 		}
-		
-		return null;
-		
+
 	}
 
-}
