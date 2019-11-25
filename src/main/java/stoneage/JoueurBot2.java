ebe5041ac699eae10d4eca9b33e32058cad73fb6
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-11-25 23:14:31 +0100

    Modification de la class JoueurBot2#98

--- src/main/java/stoneage/JoueurBot2.java
+++ src/main/java/stoneage/JoueurBot2.java
@@ -6,23 +6,23 @@ public class JoueurBot2 implements Joueurs {
 	Random rand = new Random();
 	public int gains;
 	public String TypeGains;
-        String name;
+    private String name;
 	int num;
 	JoueurBot2(String name,int num){
             this.name=name;
             this.num=num;
-        }
-        public int getNum(){
+	}
+    public int getNum(){
             return num;
         }
-		public int getGains(){
+	public int getGains(){
 		return gains;
 	}
-		public String TypeGains(){
-			return  TypeGains;
-		}
+	public String TypeGains(){
+		return  TypeGains;
+	}
 
-	 public int placerOutils(int nbOutils,int nbRessources, Zone zoneChoisi) { 
+	public int placerOutils(int nbOutils,int nbRessources, Zone zoneChoisi) {
 	    int OutilChoisie ;	
 		if ((zoneChoisi.niveauZone-(nbRessources % zoneChoisi.niveauZone))<=nbOutils)
     	{
@@ -33,12 +33,13 @@ public class JoueurBot2 implements Joueurs {
     	}
     	return OutilChoisie;
     }
-	public int cadeauRes(ArrayList<Integer> listeDe ){
+
+    public int cadeauRes(ArrayList<Integer> listeDe ){
 		//cette methode va permettre au joueur de choisir la resouce cadeau de la carte civilisation
 		return listeDe.get(rand.nextInt(listeDe.size()));
 	}
 	
-	    public int choixTypeRes(int cout,Inventaire inv, int...typeDispo) { 
+	public int choixTypeRes(int cout,Inventaire inv, int...typeDispo) {
 	        /* cette methode permet au joueure de choisir la resource qu'il va utiliser pour payer ses dettes */
 	        	ArrayList<Integer> listTypeDispo = new ArrayList(); //transforme le tableau en liste
 	        	for (int i=0 ; i<typeDispo.length; i++) {
@@ -51,38 +52,33 @@ public class JoueurBot2 implements Joueurs {
 	        	 * 5: Pierre 
 	        	 * 6: Or 
 	        	 */
-	        	if ( inv.getNbBois()<cout) {// s'il ya pas assez de bois on enleve l'indice de cette zone de la liste 
-	        		listTypeDispo.remove(0);
-	    		}
-	    		else if ( inv.getNbArgile()<cout) {
-	    			listTypeDispo.remove(1);
-	    		}
-	    		else if ( inv.getNbPierre()>cout) {
-	    			listTypeDispo.remove(2);
-	    		}
-	    		else if (inv.getNbOr()>cout) {
-	    			listTypeDispo.remove(3);
-	    		}    	
-	        	if(listTypeDispo.size()>0) {
-	        		int i=rand.nextInt((listTypeDispo.size()));
-	        		return listTypeDispo.get(i);
-	        	}
+			if ( inv.getNbBois()<cout) {
+				// s'il ya pas assez de bois on enleve l'indice de cette zone de la liste
+				listTypeDispo.remove(listTypeDispo.indexOf(3));
+			}
+			if ( inv.getNbArgile()<cout) {
+				listTypeDispo.remove(listTypeDispo.indexOf(4));
+			}
+			if ( inv.getNbPierre()<cout) {
+				listTypeDispo.remove(listTypeDispo.indexOf(5));
+			}
+			if (inv.getNbOr()<cout) {
+				listTypeDispo.remove(listTypeDispo.indexOf(6));
+			}
+			if(listTypeDispo.size()>0&&rand.nextBoolean()) { //le joueur decide de prendre cette carte si il a acces de ressource et si il a envie (true/false)
+				int i=rand.nextInt((listTypeDispo.size()));
+				return listTypeDispo.get(i);
+			}
 	        	else {
 	        		return (-1);
 	        	}
-	        } 
-	 
-	 public int choixTypeRes(int...typeDispo) {
-		 int i=rand.nextInt((typeDispo.length));
-		 return typeDispo[i];
-	    	
-	 }
+	        }
 	
-/*  **************************************************************************************** 
+	/*  ****************************************************************************************
  	* Place un ouvrier en fonction de ses ressources et de son nombre d'ouvrier dispo      *
  	* Si les conditions ne marchent pas pour placer ses ouvrier il les place aléatoirement *
     ****************************************************************************************/
-		public Choix placerOuvriers(ArrayList<Zone> LesZones ,Inventaire inv){
+	public Choix placerOuvriers(ArrayList<Zone> LesZones ,Inventaire inv){
 			if ( (inv.getNbZoneJouer() < 6 && inv.ouvrierDispo())){
 
 				if (inv.listeZonesJouer.get(1)==false && inv.getNourriture() >= 5 && inv.getNourriture() <10  && LesZones.get(1).getNbPlaceDispo()>2 )//Chasse
@@ -105,9 +101,10 @@ public class JoueurBot2 implements Joueurs {
 				else if (StoneAge.getNbJoueurTotal()==2){
 					if ( (inv.getNbZoneJouer() < 6&&inv.ouvrierDispo())){
 						int i=0;
-						int zoneChoisie = rand.nextInt(9);
-					 	while ( inv.listeZonesJouer.get(zoneChoisie)==true || LesZones.get(zoneChoisie).getNbPlaceDispo()==0|| LesZones.get(zoneChoisie).nbJoueur>=1){				 	
-					 		zoneChoisie = rand.nextInt(9);
+						int[] tabZoneDispo={1,2,3,4,5,6,7,8,9,12,13};
+						int zoneChoisie = tabZoneDispo[rand.nextInt(11)];
+					 	while ( inv.listeZonesJouer.get(zoneChoisie)==true || LesZones.get(zoneChoisie).getNbPlaceDispo()==0|| LesZones.get(zoneChoisie).nbJoueur>=1){
+							zoneChoisie = tabZoneDispo[rand.nextInt(11)];
 					 		i++;
 					 		if(i==10) {
 					 			zoneChoisie = 1;
@@ -129,9 +126,10 @@ public class JoueurBot2 implements Joueurs {
 				else if(StoneAge.getNbJoueurTotal()==3) {
 					if ( (inv.getNbZoneJouer() < 6 &&inv.ouvrierDispo())){
 						int i=0;
-					 	int zoneChoisie = rand.nextInt(10);
+						int[] tabZoneDispo={1,2,3,4,5,6,7,8,9,10,12,13,14};
+						int zoneChoisie = tabZoneDispo[rand.nextInt(13)];
 					 	while ( inv.listeZonesJouer.get(zoneChoisie)==true || LesZones.get(zoneChoisie).getNbPlaceDispo()==0|| LesZones.get(zoneChoisie).nbJoueur>=2){
-					 		zoneChoisie = rand.nextInt(10);
+							zoneChoisie = tabZoneDispo[rand.nextInt(13)];
 					 		i++;
 					 		if(i==10) {
 					 			zoneChoisie = 1;
