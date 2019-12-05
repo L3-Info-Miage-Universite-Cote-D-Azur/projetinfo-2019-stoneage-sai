744b0e6787be4a1cdcb2fca98c332fcd8dd27bc2
Ossama <ossama1998@windowslive.com>
2019-12-05 23:42:11 +0100

    Test Building #102

--- src/test/java/stoneage/BuildingTilesTest.java
+++ src/test/java/stoneage/BuildingTilesTest.java
@@ -2,7 +2,7 @@ package stoneage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,14 +21,7 @@ public class BuildingTilesTest {
 	
 	@Test
     public void addCards() {
-		assertEquals(tiles.getCards().size(), 17);
-	}
-
-	
-	@Test
-    public void shuffle() {
-		tiles.shuffle(tiles.getCards());
-		assertEquals(tiles.getCards().size(), 17);
+		assertEquals(tiles.getCards().size(), 28);
 	}
 	
 	@Test
@@ -37,14 +30,14 @@ public class BuildingTilesTest {
 		for (int i = 0; i < tiles.getCards().size(); i++) {
 			tiles.getCards().get(i).ajouterDansInventaire(inv);
 		}
-		assertEquals(inv.getCarteBat().size(), 17);
+		assertEquals(inv.getCarteBat().size(), 28);
 	}
 	
 	
 	@Test
 	void getCardScore() {
 		for (int i = 0; i < tiles.getCards().size(); i++) {
-			assertTrue(tiles.getCards().get(i).getCardScore() >= 0 && tiles.getCards().get(i).getCardScore() <= 6);
+			assertTrue(tiles.getCards().get(i).getCardScore() >= 0 && tiles.getCards().get(i).getCardScore() <= 7);
 		}
 	}
 	
@@ -52,7 +45,7 @@ public class BuildingTilesTest {
 	@Test
 	void getBuildingCost() {
 		for (int i = 0; i < tiles.getCards().size(); i++) {
-			assertTrue(tiles.getCards().get(i).getBuildingCost() >= 0 && tiles.getCards().get(i).getBuildingCost() <= 12);
+			assertTrue(tiles.getCards().get(i).getBuildingCost() >= 0 && tiles.getCards().get(i).getBuildingCost() <= 21);
 		}
 	}
 }
