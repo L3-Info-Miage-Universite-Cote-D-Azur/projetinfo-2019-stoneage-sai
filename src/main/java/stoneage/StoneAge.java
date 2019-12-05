856b635f2f700c52e344c4d4d1e85a7eb70c229c
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2019-12-05 18:34:02 +0100

    Ajout fonctionnalité des Statistique pour 500 parties #114

--- src/main/java/stoneage/StoneAge.java
+++ src/main/java/stoneage/StoneAge.java
@@ -22,10 +22,35 @@ public class StoneAge {
  	public Zone zone;
  	
  	/****** Choisir un Nombre de Joueure Pour commencer une Partie ( entre 2 et 4 )******/
+ 	/*Pour les statistique il se peut que x joueur arrive en tete avec le meme score, alors les x joueur gagne la partie.
+ 	 * C'est pour cela que le total des victoire des joueur dans les statistiques peut depasser 500.
+ 	 * Mais il y a bien 500 parties joués.
+ 	 */
+ 	
+ 	
 	public static final void main(String [] args) {
-		StoneAge stoneAge = new StoneAge(4);
-		stoneAge.jouer();
+		
+		
+		//// *** Pour 1 partie ***
+		//StoneAge stoneAge = new StoneAge(4);
+		//stoneAge.jouer();
+		
+		
+		
+		// *** Pour 500 parties avec Stat ***
+		int[][] tab = new int[4][15];
+		StoneAge stoneAge;
+		stoneAge = new StoneAge(4);
+		for (int p = 0; p < 500; p++) {
+			stoneAge = new StoneAge(4);
+			stoneAge.jouer();
+			stoneAge.calculStat(tab);
+		}
+		stoneAge.afficheStat(tab);
 	}
+	
+	
+	
 	 public StoneAge(int nbJ){
 		 nbJoueurTotal = nbJ;
 		 listeDesInventaires=new ArrayList<>() ;
@@ -45,8 +70,7 @@ public class StoneAge {
 
 	}
 	    
-	private void jouer(){
-
+	public void jouer(){
         System.out.println("***************** Debut de la Partie *****************\n");
         System.out.println("** Debut de la Partie **\n");
         System.out.println("Joueur 1 : JoueurIA, un joueur inteligent qui utilise une strategie de jeu gagante.");
@@ -65,6 +89,9 @@ public class StoneAge {
         System.out.println("***************** fin de la Partie *****************");	
         gagner();
 	}	
+	
+	
+	
     private void unTour(){
     	for (int i=0 ; i<=nbJoueurs;i++){
     		listeDesInventaires.get(i).resetAvailableWorkers(); //remettre a jour le nombre d'ouvrier disponnible
@@ -170,14 +197,13 @@ public class StoneAge {
 	    		}
 	    	}
 		}
-
 		System.out.println("**** Phase Nourrir les ouvriers ****"+ "\n");
 		for (int i=0 ; i<=nbJoueurs;i++){
 			partie.phaseNourrir( listeDesInventaires.get(i), listeDesJoueurs.get(i));
 
 		}
                 
-                for (int i=0 ; i<=nbJoueurs;i++) {
+        for (int i=0 ; i<=nbJoueurs;i++) {
 			int numJ=listeDesJoueurs.get(i).getNum();
 			System.out.println("**** Inventaire du joueur "+numJ+" ****" );
 			System.out.println("Bois  : " + listeDesInventaires.get(i).getNbBois()+
@@ -196,8 +222,9 @@ public class StoneAge {
 				Collections.swap(listeDesInventaires,i,i+1);
 				Collections.swap(listeDesJoueurs,i,i+1);
 		}
-
     }
+    
+    
 	 public void gagner() {
 	        ArrayList<Integer> listScore= new ArrayList<>();
 			for (int i=0 ; i<=nbJoueurs;i++){
@@ -254,8 +281,90 @@ public class StoneAge {
 	    		System.out.println("Le score des joueurs est negative! Ils ont tous perdu. ");
 
 	    	}
-     	    
 	 }
+	 
+	 
+	 
+	 public void calculStat(int[][] tabStatistique) {
+		 ArrayList<Integer> listScore= new ArrayList<>();
+		 for (int i=0 ; i<=nbJoueurs;i++){
+		 	listScore.add(listeDesInventaires.get(i).calcScore());
+		 }
+		 int Gagnant=0;
+	     int ScoreGagnant=0;
+		 for (int i = 0; i <= nbJoueurs; i++) {
+			    int numJ=listeDesJoueurs.get(i).getNum();
+			    tabStatistique[numJ-1][1] += listeDesInventaires.get(i).calcScore();
+			    tabStatistique[numJ-1][2] += listeDesInventaires.get(i).getNbOuvrier();
+			    tabStatistique[numJ-1][3] += listeDesInventaires.get(i).getNbBois();
+				tabStatistique[numJ-1][4] += listeDesInventaires.get(i).getNbArgile();
+				tabStatistique[numJ-1][5] += listeDesInventaires.get(i).getNbPierre();
+				tabStatistique[numJ-1][6] += listeDesInventaires.get(i).getNbOr();
+				tabStatistique[numJ-1][7] += listeDesInventaires.get(i).getNourriture();
+				tabStatistique[numJ-1][8] += listeDesInventaires.get(i).getScoreChamp();
+				tabStatistique[numJ-1][9] += listeDesInventaires.get(i).getNbConstructeur();
+			    tabStatistique[numJ-1][10] += listeDesInventaires.get(i).getNbFabricant();
+				tabStatistique[numJ-1][11] += listeDesInventaires.get(i).getNbPaysan();
+				tabStatistique[numJ-1][12] += listeDesInventaires.get(i).getNbChamane();
+				tabStatistique[numJ-1][13] += listeDesInventaires.get(i).getNbCarteVert();
+				tabStatistique[numJ-1][14] += listeDesInventaires.get(i).getNbCarteBat();
+	    		if ( listeDesInventaires.get(i).calcScore()>ScoreGagnant){
+	    			Gagnant=numJ; //si le score du joueur corant est le plus elever alors on change les deux variables.
+	    			ScoreGagnant=listeDesInventaires.get(i).calcScore();
+	    		}       
+	        }        
+    		int nbScoreEgaux=0;
+    	    ArrayList<Integer>  joueurGagnant=new ArrayList();
+	    	for (int i=0 ; i<=nbJoueurs;i++){
+	    		if (listeDesInventaires.get(i).calcScore()== ScoreGagnant) {
+	    			nbScoreEgaux++;
+	    			joueurGagnant.add(listeDesJoueurs.get(i).getNum());
+	    		}
+	    	}
+			switch (nbScoreEgaux) {
+				case 1 :
+			 		tabStatistique[Gagnant - 1][0] += 1;
+			 		break;
+			 		
+			 	case 2 :
+			 		tabStatistique[joueurGagnant.get(0)-1][0] += 1;
+			 		tabStatistique[joueurGagnant.get(1)-1][0] += 1;
+			 		break;
+			 		
+			 	case 3 :
+			 		tabStatistique[joueurGagnant.get(0)-1][0] += 1;
+			 		tabStatistique[joueurGagnant.get(1)-1][0] += 1;
+			 		tabStatistique[joueurGagnant.get(2)-1][0] += 1;
+			 		break;
+			}
+	 }
+	 
+	 
+	 public void afficheStat(int[][] tabStatistique) {
+		 for (int i = 0; i <= nbJoueurs; i++) {
+			 System.out.println("**** Statistique du joueur " + (i+1) + " ****");
+			 
+			 System.out.println("\nMoyenne sur 500 parties du joueur " + (i+1) + " : ");
+			 System.out.print("Nombre d'ouvriers : " + (tabStatistique[i][2]) / 500);
+			 System.out.print(" | Bois : " + (tabStatistique[i][3]) / 500);
+			 System.out.print(" | Argiles : " + (tabStatistique[i][4]) / 500);
+			 System.out.print(" | Pierres : " + (tabStatistique[i][5]) / 500);
+			 System.out.print(" | Or : " + (tabStatistique[i][6]) / 500);
+			 System.out.println(" | Nourriture : " + (tabStatistique[i][7]) / 500);
+			 System.out.print("Nombre de carte Civilisation Constructeur : " + (tabStatistique[i][9]) / 500); 
+			 System.out.print(" | Fabricant : " + (tabStatistique[i][10]) / 500);
+			 System.out.print(" | Paysan : " + (tabStatistique[i][11]) / 500);
+			 System.out.println(" | Chamane : " + (tabStatistique[i][12]) / 500);
+			 System.out.println("Une agriculture de niveau : " + (tabStatistique[i][8]) / 500);
+			 System.out.println("Nombre de carte Civilisation Verte " + (tabStatistique[i][13]) / 500);
+			 System.out.println("Nombre de carte Civilisation Batiment " + (tabStatistique[i][14]) / 500);
+			 System.out.println("Le Score moyen  du joueur ** " + (i+1) + " ** : " + (tabStatistique[i][1]) / 500 + " points");
+			 System.out.println("\nAu total le joueur ** " + (i+1) + " ** a remporter : " + tabStatistique[i][0] + " parties sur 500\n");
+		 }
+	 }
+	 
+	 
+	 
 	public static int getNbJoueurTotal() {
 		return nbJoueurTotal;
 	}
