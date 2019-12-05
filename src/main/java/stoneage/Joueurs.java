8398f0bff2535747550dd92e18ea22d33e55678f
Ossama <ossama1998@windowslive.com>
2019-12-05 23:26:09 +0100

    Modifcation de l'interface Joueurs#92

--- src/main/java/stoneage/Joueurs.java
+++ src/main/java/stoneage/Joueurs.java
@@ -13,11 +13,13 @@ import java.util.Map;
  * @author Ossama
  */
 public interface Joueurs {
-    public int cadeauRes(ArrayList<Integer> listeDe );
+        public int cadeauRes(ArrayList<Integer> listeDe );
 	public int choixTypeRes(int cout,Inventaire inv, int...typeDispo) ;
 	public int placerOutils(int nbOutils,int nbRessources, Zone zoneChoisi) ;
 	public Choix placerOuvriers(ArrayList<Zone> LesZones ,Inventaire inv);
 	public int getNum();
     public Map NourrirOuv(Inventaire inv ,  int nm);
     public boolean payerBatiment();
+    public ArrayList<Integer> payBuildingWith(Inventaire inv,int cout,int types);
+    public void resolution(Inventaire inv,ArrayList<Integer> res,int cout);
 }
