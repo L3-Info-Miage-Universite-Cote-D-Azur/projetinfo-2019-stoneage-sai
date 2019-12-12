3afdbc4dea68c32dec11673b29cd02f646e8324d
Yasmine Ben Fredj <yasmine.ben-fredj@etu.unice.fr>
2019-12-12 20:19:29 +0100

    Amélioration complexité #105: la class inventaire

--- src/main/java/stoneage/Inventaire.java
+++ src/main/java/stoneage/Inventaire.java
@@ -1,27 +1,20 @@
 package stoneage;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.Set;
+
+import java.util.*;
 
 /*
  * 	public class Inventaire contient:
- *  Un nombre d'ouvriers, 
+ *  Un nombre d'ouvriers,
  *  Des ressources de différents types,
- *  Methodes qui permettent d'attribuer des valeur et accéder à ces valeurs 
- *  
+ *  Methodes qui permettent d'attribuer des valeur et accéder à ces valeurs
+ *
  */
 public class Inventaire {
 	public static final int NB_INITIAL_OUVRIERS = 5;
+	public Map<Integer, Ressources> lesRessources = new HashMap<>();
 	private int nbOuvrier = 5; //Minimum 1 ouvrier pour pouvoir jouer
-	private int nbRessource = 0;
 	private int nbOuvrierDispo = 0;
-	private int nbBois;
-	private int nbArgile;
-	private int nbPierre;
-	private int nbOr;
-	private int nbNourriture;
-	private int nbOutils;
 	private int nbOutilsDutour;  //nombre d'outil qui va etre minorer dans le tour et revient au nombre d'outil initial en fin de tour
 	private int scoreChamp;
 	private int nbPaysan;// va etre multiplier par le scoreChamp pour le score final
@@ -38,22 +31,23 @@ public class Inventaire {
 	public ArrayList<Boolean> listeZonesJouer;
 	public ArrayList<Integer> listeOuvriersPlaces;
 	public Inventaire() {  //Initialisation d'un Inventaire vide
-		setNbOuvrier(NB_INITIAL_OUVRIERS); //Initialisation du nombre d'ouvrier 
+
+		this.lesRessources.put(1,new Ressources(1,"Outil",0));
+		this.lesRessources.put(2,new Ressources(2,"Nourriture",12));
+		this.lesRessources.put(3,new Ressources(3,"Bois",0));
+		this.lesRessources.put(4,new Ressources(4,"Argile",0));
+		this.lesRessources.put(5,new Ressources(5,"Pierre",0));
+		this.lesRessources.put(6,new Ressources(6,"Or",0));
+		setNbOuvrier(NB_INITIAL_OUVRIERS); //Initialisation du nombre d'ouvrier
+
 		// Initialisation à 0 des ressources et du score
-		setNbBois(0);
-		setNbArgile(0);
-		setNbPierre(0);
-		setNbOr(0);
-		setNourriture(15);
-		setNbRessource(0);
-		setNbOutils(0);
 		setScore(0);
 		setScoreChamp(0);
 		resetAvailableWorkers();
 		listeDesCarteCivilisation = new ArrayList<>();
 		listeDesCarteBatiments=new ArrayList<>();
 		listeOuvriersPlaces=new ArrayList<>();
-		setTypeCarteCivVerte = new LinkedHashSet<>();	
+		setTypeCarteCivVerte = new LinkedHashSet<>();
 		listeZonesJouer  = new ArrayList<>();
 		listeZonesDispo = new ArrayList<>();
 		for (int i=1;i < 17;i++ ){
@@ -63,29 +57,22 @@ public class Inventaire {
 		} //remplire la liste des zones
 	}
 
-    public Inventaire(Inventaire toCopy) {
-        setNbOuvrier(toCopy.getNbOuvrier());
-        setNbRessource(toCopy.getNbRessource());
-        resetAvailableWorkers();
-    }
+	public Inventaire(Inventaire toCopy) {
+		setNbOuvrier(toCopy.getNbOuvrier());
+		resetAvailableWorkers();
+	}
 	public boolean ouvrierDispo() {
 		return (nbOuvrierDispo > 0);
 	}
 
 	public void resetInventory(){ //sert pour le teste
-		setNbArgile(0);
-		setNbPierre(0);
-		setNbOr(0);
-		setNourriture(15);
-		setNbRessource(0);
-		setNbOutils(0);
 		setScore(0);
 		setScoreChamp(0);
 		resetAvailableWorkers();
 	}
 	/* ****************************************
-	   * Getter and setter and reset  des champs privés  *
-	   **************************************** */
+	 * Getter and setter and reset  des champs privés  *
+	 **************************************** */
 
 	// La methode retourne le nombre de zone que le joueur a choisi durant le tour, elle est renisialiser en fin de tour
 	public int getNbZoneJouer(){
@@ -108,65 +95,45 @@ public class Inventaire {
 	public int getNbOuvrier() {
 		return nbOuvrier;
 	}
-	
+
 	public void setNbOuvrier(int nbOuvrier) {
 		this.nbOuvrier = nbOuvrier;
 	}
-	
+
 	public int getNbRessource() {
-		return nbRessource;
-	}
-	
-	public void setNbRessource(int nbRessource) {
-		this.nbRessource = nbRessource;
+		return lesRessources.get(3).getValeur()+lesRessources.get(5).getValeur()+lesRessources.get(4).getValeur()+lesRessources.get(6).getValeur();
 	}
 
 	public int getNbBois() {
-		return nbBois;
+		return lesRessources.get(3).getValeur();
+	}
+	public int getNbOr() {
+		return lesRessources.get(6).getValeur();
 	}
 
-	public void setNbBois(int nbBois) {
-		this.nbBois = nbBois;
-	}
 	public int getNbArgile() {
-		return nbArgile;
-	}
-	public void setNbArgile(int nbArgile) {
-		this.nbArgile = nbArgile;
+		return lesRessources.get(4).getValeur();
 	}
 
 	public int getNbPierre() {
-		return nbPierre;
+		return lesRessources.get(5).getValeur();
 	}
 
-	public void setNbPierre(int nbPierre) {
-		this.nbPierre = nbPierre;
-	}
-
-	public int getNbOr() {
-		return nbOr;
-	}
-
-	public void setNbOr(int nbOr) {
-		this.nbOr = nbOr;
-	}
-	public int getNourriture() {
-		return nbNourriture;
-	}
-	public void setNourriture(int nbNourriture) {
-		this.nbNourriture = nbNourriture;
+	public int getNourriture()  {
+		return lesRessources.get(2).getValeur();
 	}
 	public int getNbOutils(){
-		return nbOutils;
+		return lesRessources.get(1).getValeur();
 	}
+
+	public void setNourriture(int nb){
+		lesRessources.get(2).addvaleur(nb);
+	}
+
 	public int getNbOutilsDuTour(){
 		return nbOutilsDutour;
 	}
-	public void setNbOutils(int nbOutils){
-		this.nbOutilsDutour+=(this.nbOutils-nbOutils);
-		//on ajoute un nouvel outil pour nb outildu tou et nb outil final si le joueur en gagne un pendant le tour
-		this.nbOutils=nbOutils;
-	}
+
 	public void setNbOutilsDuTour(int nbOutils){
 		this.nbOutilsDutour=nbOutils;
 	}
@@ -174,19 +141,20 @@ public class Inventaire {
 		return scoreChamp;
 	}
 	public void resetNbOutilsDuTour(){
-		this.nbOutilsDutour=this.nbOutils;
+		this.nbOutilsDutour= lesRessources.get(1).getValeur();
 	}
+
 	public void setScoreChamp(int niveauChamp) {
 		scoreChamp=niveauChamp;
 	}
 	public void addCarteBat(BuildingTiles building){
-            listeDesCarteBatiments.add(building);
+		listeDesCarteBatiments.add(building);
 	}
-	
+
 	public ArrayList<BuildingTiles> getCarteBat() {
 		return listeDesCarteBatiments;
 	}
-	
+
 	public int getScore() {
 		return score;
 	}
@@ -196,17 +164,17 @@ public class Inventaire {
 
 	//La methode calcScore calcule et retoune le score finale du joueur
 	public int calcScore() {
-		return score+nbRessource+(nbOuvrier*nbChamane+nbFabricant*nbOutils+nbConstructeur*getNbCarteBat()+nbPaysan*scoreChamp)+((int)Math.pow(setTypeCarteCivVerte.size(),2))+(nbCarteVert-setTypeCarteCivVerte.size());
+		return score+getNbRessource()+(nbOuvrier*nbChamane+nbFabricant*lesRessources.get(1).getValeur()+nbConstructeur*getNbCarteBat()+nbPaysan*scoreChamp)+((int)Math.pow(setTypeCarteCivVerte.size(),2))+(nbCarteVert-setTypeCarteCivVerte.size());
 		/* Le score finale contient :
-		 * Le niveau de score 
-		 * le nombre des ressource 
+		 * Le niveau de score
+		 * le nombre des ressource
 		 * Le nombre d'ouvrier * le nombre carte Chamane
 		 * Le nombre d'outil * le nombre de carte Fabricant
-		 * Le nombre de batiment * le nombre de carte constructeur 
+		 * Le nombre de batiment * le nombre de carte constructeur
 			//nb batiment = 1 pour l'instant carte pas de carte batiment a l'iteration 4
 		 * Le nombre de carte payson * le niveau de champ
-		 * Le nombre de type de carte verte **2 
-		 * le nombre du reste des carte verte 
+		 * Le nombre de type de carte verte **2
+		 * le nombre du reste des carte verte
 		 */
 	}
 
