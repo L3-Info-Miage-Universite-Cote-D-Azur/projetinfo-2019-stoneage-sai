20103ae1a7cb749401566aacbf57e3d29527b68d
Sebastien <marrosebastien@protonmail.com>
2019-12-14 23:18:47 +0100

    Ajout de la javadoc de la classe Choix #105 #120

--- src/main/java/stoneage/Choix.java
+++ src/main/java/stoneage/Choix.java
@@ -1,25 +1,39 @@
 package stoneage;
 
+/**
+ * Cette classe représente le choix d'une zone.
+ * @see int#zoneChoisie:
+ * 		Entier qui correspond à la zone choisie
+ * 		Valeur comprise entre 0 et 15
+ * @see int#nbOuvriersChoisie
+ * 		Nombre d'ouvriers choisis pour les placer sur une zone.
+ */
 public class Choix {
 	public int zoneChoisie;
 	public int nbOuvriersChoisie;
-	
-	public Choix(int zoneJouer, int nbOuvriersJouer) { //constructeur qui fait :
-		this.zoneChoisie = zoneJouer;               //l'affectation de la zoneChoisie 
-		this.nbOuvriersChoisie = nbOuvriersJouer;   //l'affectation des nbOuvriersChoisie
+
+	/**
+	 * Constructeur de la classe Choix
+	 * @param zoneJouer:
+	 *                    Zone choisie pour placer les ouvriers
+	 * @param nbOuvriersJouer:
+	 *                       Nombre d'ouvriers à placer.
+	 *
+	 */
+	public Choix(int zoneJouer, int nbOuvriersJouer) {
+		this.zoneChoisie = zoneJouer;
+		this.nbOuvriersChoisie = nbOuvriersJouer;
 		
 	}
-	 
 	public boolean equals(Object o) {
 	     if (o instanceof Choix) {
-	    	 
 	         Choix c = (Choix) o;
 	            return (c.nbOuvriersChoisie == nbOuvriersChoisie) && (c.zoneChoisie == zoneChoisie);
 	        }
 	        else return false;
-	    }
-        @Override
-         public String toString(){
+	}
+	@Override
+	public String toString(){
              return "nbOuvriersChoisie="+nbOuvriersChoisie+"  Indice de la zone Choisie="+(zoneChoisie+1);
          }
 }
