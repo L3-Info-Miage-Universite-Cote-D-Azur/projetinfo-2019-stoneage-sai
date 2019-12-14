b17a0571460e0b4962ffb55d2d5bfaee319466bf
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2019-12-14 02:06:48 +0100

    Modification des tests pour la class Partie #118

--- src/test/java/stoneage/PartieTest.java
+++ src/test/java/stoneage/PartieTest.java
@@ -11,15 +11,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PartieTest {
 	private  Partie partie;
-	Zone zone= new Zone();
+	Zone zone = new Zone(2, 100, "Chasse");
 
-    private Inventaire  inv,inv2;
+    private Inventaire  inv, inv2, inv3;
     private Joueurs joueur, joueurIA, joueurBot2;
    
     @BeforeEach
     void setUp(){
     	inv = new Inventaire();
     	inv2 = new Inventaire();
+    	inv3 = new Inventaire();
     	joueur = new Joueur("oss",1);
     	joueurIA = new JoueurIA("oss",2);
     	joueurBot2 = new JoueurBot2("oss", 2);
@@ -43,11 +44,12 @@ public class PartieTest {
 	
 	@Test
 	public void testPhasePlacement() {
-		inv.setNourriture(0);
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",0));    //On initialise le nombre de nourriture a 0 dans l'inventaire du joueur
 		partie.phasePlacement(inv, joueurBot2); //Comme le JoueurBot2 n'a pas de nourriture il doit jouer la zone chasse
 		assertTrue(inv.listeZonesJouer.get(1)); //"listeZonesJouer.get(1)" est la zone chasse
 		
-		inv2.setNourriture(20);
+
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",15));  //On initialise a 15
 		partie.phasePlacement(inv2, joueurBot2); //Comme le JoueurBot2 a de la nourriture il ne va pas choisir la zone chasse
 		assertFalse(inv2.listeZonesJouer.get(1));
 	}
@@ -57,89 +59,91 @@ public class PartieTest {
 	public void testPhaseNourrir() {
 		joueur = new Joueur("oss",3);
 		
-
 		partie.phaseNourrir(inv, joueur);
-		assertEquals(inv.getNourriture(), 10);
+		assertEquals(inv.getNourriture(), 7);
 		
 		inv.resetInventory();
-		inv.setNourriture(0);
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",0));
 		inv.setScore(30);     //On initialise le score du joueur a 30
 		partie.phaseNourrir(inv, joueur);  //Comme le joueur n'a pas assez de nourriture et de ressource pour nourrire ses ouvriers, il a une pénalité de 10 points
 		assertEquals(inv.getScore(), 20);     //Le score du joueur doit donc etre de 20
 		
-		
 		inv.resetInventory();
-		inv.setNourriture(1);
-		inv.setNbBois(10);
-		inv.setNbRessource(10);
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",1));
+		inv.lesRessources.replace(3, new Ressources(3,"Bois",10));
 		partie.phaseNourrir(inv, joueur);  //Le joueur utilise sont bois pour nourrire ses ouvriers
 		assertEquals(inv.getNbBois(), 6);
 		assertEquals(inv.getNbRessource(), 6);
 		
 		inv.resetInventory();
-		inv.setNourriture(0);
-		inv.setNbBois(3);
-		inv.setNbArgile(10);
-		inv.setNbRessource(13);
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",0));
+		inv.lesRessources.replace(3, new Ressources(3,"Bois",3));
+		inv.lesRessources.replace(4, new Ressources(4,"Argile",10));
 		partie.phaseNourrir(inv, joueur);  //Le joueur utilise 3 bois et 2 argiles pour nourrire ses ouvriers
 		assertEquals(inv.getNbBois(), 0);
 		assertEquals(inv.getNbArgile(), 8);
 		assertEquals(inv.getNbRessource(), 8);
 		
-		
 		inv.resetInventory();
-		inv.setNourriture(0);
-		inv.setNbBois(1);
-		inv.setNbArgile(1);
-		inv.setNbPierre(1);
-		inv.setNbOr(10);
-		inv.setNbRessource(13);
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",0));
+		inv.lesRessources.replace(3, new Ressources(3,"Bois",1));
+		inv.lesRessources.replace(4, new Ressources(4,"Argile",1));
+		inv.lesRessources.replace(5, new Ressources(5,"Pierre",1));
+		inv.lesRessources.replace(6, new Ressources(6,"Or",10));
 		partie.phaseNourrir(inv, joueur);  //Le joueur utilise le bois, l'argile, la pierre et 2 or pour nourrire ses ouvriers
 		assertEquals(inv.getNbBois(), 0);
 		assertEquals(inv.getNbArgile(), 0);
 		assertEquals(inv.getNbPierre(), 0);
 		assertEquals(inv.getNbOr(), 8);
-		
-		
+		assertEquals(inv.getNbRessource(), 8);
 		
 		inv.resetInventory();
-		inv.setNourriture(1);
-		inv.setNbBois(0);
-		inv.setNbArgile(1);
-		inv.setNbPierre(4);
-		inv.setNbOr(10);
-		inv.setNbRessource(15);
+		inv.lesRessources.replace(2, new Ressources(2,"Nourriture",1));
+		inv.lesRessources.replace(3, new Ressources(3,"Bois",0));
+		inv.lesRessources.replace(4, new Ressources(4,"Argile",1));
+		inv.lesRessources.replace(5, new Ressources(5,"Pierre",4));
+		inv.lesRessources.replace(6, new Ressources(6,"Or",10));
 		partie.phaseNourrir(inv, joueur);
 		assertEquals(inv.getNbBois(), 0);
 		assertEquals(inv.getNbArgile(), 0);
 		assertEquals(inv.getNbPierre(), 1);
 		assertEquals(inv.getNbOr(), 10);
 		assertEquals(inv.getNbRessource(), 11);
-		
 	}
+	
+	
 	
 	
 	
 	@Test
-	@Disabled
 	public void TestDemanderCadeaux() {
 		inv = new Inventaire();
 		inv2 = new Inventaire();
-		ArrayList<Joueurs> listeJoueurs =new ArrayList<>();
-	    ArrayList<Inventaire> listeInventaires=new ArrayList<>();
+		inv3 = new Inventaire();
+		
+		ArrayList<Joueurs> listeJoueurs = new ArrayList<>();
+	    ArrayList<Inventaire> listeInventaires = new ArrayList<>();
 	    
 	    listeJoueurs.add(joueurBot2);
+	    listeJoueurs.add(joueurIA);
+	    listeJoueurs.add(joueur);
+	    
 	    listeInventaires.add(inv);
+	    listeInventaires.add(inv2);
+	    listeInventaires.add(inv3);
     	
 		// verifier que les joueurs n'ont pas de ressource ni outil ni nivau champ 
-		assertTrue(inv2.getNbRessource()==0 &&inv2.getNbOutils()==0 &&inv2.getScoreChamp()==0 );    
-		assertTrue(inv.getNbRessource()==0 &&inv.getNbOutils()==0 &&inv.getScoreChamp()==0 );
-
+	    assertTrue(inv.getNbRessource() == 0 && inv.getNbOutils() == 0 && inv.getScoreChamp() == 0);
+		assertTrue(inv2.getNbRessource() == 0 && inv2.getNbOutils() == 0 && inv2.getScoreChamp() == 0);    
+		assertTrue(inv3.getNbRessource() == 0 && inv3.getNbOutils() == 0 && inv3.getScoreChamp() == 0);
+		
 		// verifier que les joueur on ressus leur cadeau 
 		Partie.demanderCadeau(zone,listeInventaires, listeJoueurs, joueurBot2, inv2);
-		assertTrue(inv2.getNbRessource()==1 ||inv2.getNbOutils()==1 ||inv2.getScoreChamp()==1 );   
-		assertTrue(inv.getNbRessource()==1 ||inv.getNbOutils()==1 ||inv.getScoreChamp()==1 );
+		assertTrue(inv.getNbRessource() == 1 || inv.getNbOutils() == 1 || inv.getScoreChamp() == 1);
+		assertTrue(inv2.getNbRessource() == 1 || inv2.getNbOutils() == 1 || inv2.getScoreChamp() == 1);   
+		assertTrue(inv3.getNbRessource() == 1 || inv3.getNbOutils() == 1 || inv3.getScoreChamp() == 1);
 	}
+	
 	
 	
 }
