96f116cd4458f8c1ed45c2177a6d7649756eaf31
Sebastien <marrosebastien@protonmail.com>
2019-12-14 23:34:39 +0100

    Ajout javadoc Main1 et Main2 #105 #120

--- src/main/java/stoneage/Main1.java
+++ src/main/java/stoneage/Main1.java
@@ -1,16 +1,10 @@
 package stoneage;
 
-
-
+/**
+ * Lancement d'une unique partie avec le choix du nombre de joueurs (entre 2 et 4).
+ */
 public class Main1 {
-
 	public static final void main(String [] args) {
-
-		/****** Choisir "unePartie(nbJoueur)" pour lancer une seule partie.
-		 * **** Choisir "partie500Stat(nbJoueur)" pour lancer 500 partie avec les statistiques.
-		 * **** nbJoueur = Le nombre de joueur compris entre 2 et 4. ******/
-
-		
 		if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3|| Integer.parseInt(args[0]) == 4) {
 			StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), false);
 			stoneage.unePartie(Integer.parseInt(args[0]));
--- src/main/java/stoneage/Main2.java
+++ src/main/java/stoneage/Main2.java
@@ -1,18 +1,10 @@
 package stoneage;
 
-
-
+/**
+ * Lancement de 500 parties avec le choix du nombre de joueurs (entre 2 et 4).
+ */
 public class Main2 {
-
 	public static final void main(String [] args) {
-
-		/**** Choisir "unePartie(nbJoueur)" pour lancer une seule partie.
-		 * Choisir "partie500Stat(nbJoueur)" pour lancer 500 partie avec les statistiques.
-		 * nbJoueur = Le nombre de joueur compris entre 2 et 4. 
-		 * Pour les statistique il se peut que x joueur arrive en tete avec le meme score, alors les x joueur gagne la partie.
-		 * C'est pour cela que le total des victoire des joueur dans les statistiques peut depasser 500.
-		 * Mais il y a bien 500 parties jouées. ******/
-		
 		if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3|| Integer.parseInt(args[0]) == 4) {
 			StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), true);
 			stoneage.partie500Stat(Integer.parseInt(args[0]));
