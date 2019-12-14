f59c4ed3ed3c8b9897cf776e0af2ac5d6c8917f0
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2019-12-14 19:16:48 +0100

    Exécution en fonction du nombre de joueur #105

--- src/main/java/stoneage/Main1.java
+++ src/main/java/stoneage/Main1.java
@@ -10,7 +10,15 @@ public class Main1 {
 		 * **** Choisir "partie500Stat(nbJoueur)" pour lancer 500 partie avec les statistiques.
 		 * **** nbJoueur = Le nombre de joueur compris entre 2 et 4. ******/
 
-		StoneAge stoneage = new StoneAge(4, false);
-		stoneage.unePartie(4);
+		
+		if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3|| Integer.parseInt(args[0]) == 4) {
+			StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), false);
+			stoneage.unePartie(Integer.parseInt(args[0]));
+		}
+		else {
+			// Execution par defaut
+			StoneAge stoneage = new StoneAge(4, false);
+			stoneage.unePartie(4);
+		}
 	}
 }
--- src/main/java/stoneage/Main2.java
+++ src/main/java/stoneage/Main2.java
@@ -12,8 +12,15 @@ public class Main2 {
 		 * Pour les statistique il se peut que x joueur arrive en tete avec le meme score, alors les x joueur gagne la partie.
 		 * C'est pour cela que le total des victoire des joueur dans les statistiques peut depasser 500.
 		 * Mais il y a bien 500 parties jouées. ******/
-
-		StoneAge stoneage = new StoneAge(4, true);
-		stoneage.partie500Stat(4);
+		
+		if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3|| Integer.parseInt(args[0]) == 4) {
+			StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), true);
+			stoneage.partie500Stat(Integer.parseInt(args[0]));
+		}
+		else {
+			// Execution par defaut
+			StoneAge stoneage = new StoneAge(4, true);
+			stoneage.partie500Stat(4);
+		}
 	}
 }
