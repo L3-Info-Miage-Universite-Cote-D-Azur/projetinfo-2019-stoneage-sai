6bbf91166f74746f00c8b98a7eca1c5580ee2366
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2019-12-15 13:36:16 +0100

    Amélioration pour l’exécution des partie #105

--- src/main/java/stoneage/Main1.java
+++ src/main/java/stoneage/Main1.java
@@ -5,11 +5,17 @@ package stoneage;
  */
 public class Main1 {
 	public static final void main(String [] args) {
-		if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3|| Integer.parseInt(args[0]) == 4) {
-			StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), false);
-			stoneage.unePartie(Integer.parseInt(args[0]));
+		try {
+			if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3 || Integer.parseInt(args[0]) == 4) {
+				StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), false);
+				stoneage.unePartie(Integer.parseInt(args[0]));
+			}
+			else {
+				StoneAge stoneage = new StoneAge(4, false);
+				stoneage.unePartie(4);
+			}
 		}
-		else {
+		catch (Exception e) {
 			// Execution par defaut
 			StoneAge stoneage = new StoneAge(4, false);
 			stoneage.unePartie(4);
--- src/main/java/stoneage/Main2.java
+++ src/main/java/stoneage/Main2.java
@@ -5,11 +5,17 @@ package stoneage;
  */
 public class Main2 {
 	public static final void main(String [] args) {
-		if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3|| Integer.parseInt(args[0]) == 4) {
-			StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), true);
-			stoneage.partie500Stat(Integer.parseInt(args[0]));
+		try {
+			if (Integer.parseInt(args[0]) == 2 || Integer.parseInt(args[0]) == 3 || Integer.parseInt(args[0]) == 4) {
+				StoneAge stoneage = new StoneAge(Integer.parseInt(args[0]), true);
+				stoneage.partie500Stat(Integer.parseInt(args[0]));
+			}
+			else {
+				StoneAge stoneage = new StoneAge(4, true);
+				stoneage.partie500Stat(4);
+			}
 		}
-		else {
+		catch (Exception e) {
 			// Execution par defaut
 			StoneAge stoneage = new StoneAge(4, true);
 			stoneage.partie500Stat(4);
